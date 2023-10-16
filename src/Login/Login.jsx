4c6e9723d31a9bb89323b0c19f0194858d944ecb
Shuvo <mahbubshahriar99@gmail.com>
2023-10-16 21:48:05 +0600

    update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./login.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FcGoogle } from "react-icons/fc";
 
 
