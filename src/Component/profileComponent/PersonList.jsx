0a4ce6e0e273bde63cb3d8641698e58508645aa4
amit-datta <shuvodatta1128@gmail.com>
2023-11-04 12:54:16 +0600

    updated

--- src/Component/profileComponent/PersonList.jsx
+++ src/Component/profileComponent/PersonList.jsx
@@ -1,24 +1,53 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, CSSProperties } from "react";
 import ProfileComponent from "./ProfileComponent";
 import { AuthContext } from "../../Providers/AuthProviders";
+import BarLoader from "react-spinners/BarLoader";
+import { Link } from "react-router-dom";
 
 const PersonList = ({ searchString }) => {
+  const userId = localStorage.getItem("userID");
+  console.log(userId);
+  // Check if the user is logged in based on the presence of "UserID" in localStorage
+  const isLoggedIn = !!userId; // Convert to a boolean
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "#4C40ED",
+  };
   const { user } = useContext(AuthContext);
-
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#4C40ED");
   const x = searchString;
-  console.log(x);
+  // console.log(x);
 
   const apiUrl = `http://localhost:5000/providers/providers?category=${x}`; // Replace with your API endpoint
   const [dataArray, setDataArray] = useState([]);
   // const [dataArray2, setDataArray2] = useState([]);
   const [dataArray3, setDataArray3] = useState([]);
+  const [colorIndex, setColorIndex] = useState(0);
+  const predefinedColors = ["#4C40ED", "#0000FF", "#ab20fd"]; // Define an array of color
+  // Function to update the color state
+  const updateColor = () => {
+    setColorIndex((prevIndex) => (prevIndex + 1) % predefinedColors.length);
+  };
+
+  useEffect(() => {
+    // Use the setInterval function to update the color every 1 second
+    const colorInterval = setInterval(() => {
+      updateColor();
+    }, 1500);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(colorInterval);
+  }, []);
 
   useEffect(() => {
     // Use the useEffect hook to fetch data when the component mounts
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        setDataArray(data); // Update the state with the fetched data
+        setDataArray(data);
+        // Update the state with the fetched data
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -44,21 +73,23 @@ const PersonList = ({ searchString }) => {
   const userLat = dataArray3.map((item) => item.user_lat);
   const userLon = dataArray3.map((item) => item.user_lon);
 
-  // const apiUrl2 = `http://localhost:5000/providers/providersProfile/${userEmail}`; // Replace with your API endpoint
-
-  // useEffect(() => {
-  //   fetch(apiUrl2)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setDataArray2(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
-
   if (!user || dataArray3.length === 0) {
-    return <div>Loading...</div>;
+    return (
+      <div style={{ marginLeft: "50%", marginTop: "2%" }}>
+        {isLoggedIn ? (
+          <BarLoader
+            color={predefinedColors[colorIndex]}
+            loading={loading}
+            css={override}
+            size={9}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        ) : (
+          <p style={{marginLeft:"-25%"}}>Please <Link to={"/login"}><span style={{color:"#4C40ED"}}>Login</span></Link> or <Link to={"/reg"}><span style={{color:"#4C40ED"}}>Signup</span></Link> first</p>
+        )}
+      </div>
+    );
   }
 
   // let result = "South Badda, Gulshan, Dhaka";
@@ -82,12 +113,10 @@ const PersonList = ({ searchString }) => {
 
   function haversine(lat1, lon1, lat2, lon2) {
     const R = 6371; // Radius of the Earth in kilometers
-
     const lat1Rad = toRadians(lat1);
     const lon1Rad = toRadians(lon1);
     const lat2Rad = toRadians(lat2);
     const lon2Rad = toRadians(lon2);
-
     const dlon = lon2Rad - lon1Rad;
     const dlat = lat2Rad - lat1Rad;
 
